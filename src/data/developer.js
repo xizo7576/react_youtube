90da838d781b7339f274a11c9231ca1c6b17b6f4
hyoung jong park <hyoungjongpark@hyoungui-Mac-Studio.local>
2024-01-19 15:01:09 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/data/developer.js
+++ src/data/developer.js
@@ -1,6 +1,6 @@
 export const developerText = [
     {
-        img: "https://yt3.googleusercontent.com/JU6k565OVstxx_h_7TbzE1aNXKzcxKl33zjPV4p649pqsgS7E3vb0meX9DB1_D_KviDoc4xM=s176-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/JuRcVt9OFQgqh7UL1LjihpVLEbjdNXt3tGq-IQfqRMT8wVXgWg_tzyz0S_GVsgqkB3ucBC5fqeY=s176-c-k-c0x00ffffff-no-rj",
         name: "webstoryboy",
         channelAddress: "https://www.youtube.com/@webstoryboy",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw"
